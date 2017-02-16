48872b3c6df0c2aef4ff34f63ff6d3d06db91b9d
XenusRUS <xenus.doc@yandex.ru>
2017-02-16 16:52:23 +0500

    5
    5

---
+++ LAB5_DOT_NET/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB5_DOT_NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"out.txt";
+
+            try
+            {
+                Console.WriteLine("******считываем весь файл********");
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    Console.WriteLine(sr.ReadToEnd()); //ReadToEnd: считывает весь текст из файла
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("******считываем построчно********");
+                using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null) // ReadLine: считывает одну строку в файле
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("******считываем блоками********");
+                using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+                {
+                    char[] array = new char[4];
+                    // считываем 4 символа
+                    sr.Read(array, 0, 4); // Read: считывает и возвращает следующий символ в численном представлении.
+
+                    Console.WriteLine(array);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
